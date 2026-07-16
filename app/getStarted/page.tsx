b1da9967612b6bcf40002c94fd67007b7aca7d0b
Siddhant-Sharma1009 <siddhantsharma1009@gamil.com>
2026-07-16 16:05:46 +0530

    2nd push

--- app/getStarted/page.tsx
+++ app/getStarted/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useState } from 'react';
-import Link from 'next/link';
 import { ArrowRight, Terminal, User, Users, CheckCircle2, Shield } from 'lucide-react';
 
 export default function GetStartedPage() {
